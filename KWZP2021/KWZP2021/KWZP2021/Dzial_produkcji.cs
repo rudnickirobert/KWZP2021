65cb884f6fb9a592a59cca862b77b3890d611dc3
Bartek <bartoszgebicki@gmail.com>
2021-04-27 13:47:03 +0200

    Dodałem formularz główny z przyciskami, które otwierają formularze główne każdego działu

---
+++ KWZP2021/KWZP2021/KWZP2021/Dzial_produkcji.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KWZP2021
+{
+    public partial class Dzial_produkcji : Form
+    {
+        DrukarniaEntities database;
+        public Dzial_produkcji(DrukarniaEntities database)
+        {
+            InitializeComponent();
+            this.database = database;
+        }
+
+        private void Dzial_produkcji_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- KWZP2021/KWZP2021/KWZP2021/Form1.Designer.cs
+++ KWZP2021/KWZP2021/KWZP2021/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DZ_Button = new System.Windows.Forms.Button();
+            this.DM_button = new System.Windows.Forms.Button();
+            this.DP_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DZ_Button
+            // 
+            this.DZ_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.DZ_Button.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.DZ_Button.Location = new System.Drawing.Point(23, 142);
+            this.DZ_Button.Name = "DZ_Button";
+            this.DZ_Button.Size = new System.Drawing.Size(153, 82);
+            this.DZ_Button.TabIndex = 0;
+            this.DZ_Button.Text = "Dział zarządzania";
+            this.DZ_Button.UseVisualStyleBackColor = false;
+            this.DZ_Button.Click += new System.EventHandler(this.DZ_Button_Click);
+            // 
+            // DM_button
+            // 
+            this.DM_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.DM_button.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.DM_button.Location = new System.Drawing.Point(236, 143);
+            this.DM_button.Name = "DM_button";
+            this.DM_button.Size = new System.Drawing.Size(153, 81);
+            this.DM_button.TabIndex = 1;
+            this.DM_button.Text = "Dział magazynowy";
+            this.DM_button.UseVisualStyleBackColor = false;
+            this.DM_button.Click += new System.EventHandler(this.DM_button_Click);
+            // 
+            // DP_Button
+            // 
+            this.DP_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.DP_Button.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DP_Button.Location = new System.Drawing.Point(458, 142);
+            this.DP_Button.Name = "DP_Button";
+            this.DP_Button.Size = new System.Drawing.Size(140, 82);
+            this.DP_Button.TabIndex = 2;
+            this.DP_Button.Text = "Dział produkcji";
+            this.DP_Button.UseVisualStyleBackColor = false;
+            this.DP_Button.Click += new System.EventHandler(this.DP_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(635, 457);
+            this.Controls.Add(this.DP_Button);
+            this.Controls.Add(this.DM_button);
+            this.Controls.Add(this.DZ_Button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button DZ_Button;
+        private System.Windows.Forms.Button DM_button;
+        private System.Windows.Forms.Button DP_Button;
     }
 }
 
