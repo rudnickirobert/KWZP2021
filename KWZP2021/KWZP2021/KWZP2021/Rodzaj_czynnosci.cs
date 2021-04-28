8d9fc02706169864ce43a3a716d14244cbed36cc
DESKTOP-0MKNBJS\Dawidek <dawkry.1996@gmail.com>
2021-04-28 08:54:57 +0200

    Dzialajace zmianki

--- KWZP2021/KWZP2021/KWZP2021/Rodzaj_czynnosci.cs
+++ KWZP2021/KWZP2021/KWZP2021/Rodzaj_czynnosci.cs
@@ -42,7 +42,7 @@ namespace KWZP2021
         private void btnAddRodzajCzynnosci_Click(object sender, EventArgs e)
         {
             DP_Rodzaj_czynnosci newRodzajCzynnosci = new DP_Rodzaj_czynnosci();
-            newRodzajCzynnosci.rodzaj_czynnosci = this.txtNewRodzajCzynnosci.Text;
+            newRodzajCzynnosci.nazwa = this.txtNewRodzajCzynnosci.Text;
 
             this.database.DP_Rodzaj_czynnosci.Add(newRodzajCzynnosci);
             this.database.SaveChanges();
@@ -71,7 +71,7 @@ namespace KWZP2021
 
             DP_Rodzaj_czynnosci toRemove = this.database.DP_Rodzaj_czynnosci.Where(rodzaj_czynnosci => rodzaj_czynnosci.Id_rodzaj_czynnosci == id).First();
 
-            toRemove.rodzaj_czynnosci = txtNewRodzajCzynnosci.Text; // UPDATE
+            toRemove.nazwa = txtNewRodzajCzynnosci.Text; // UPDATE
 
             this.database.SaveChanges();
             initDataGridView();
