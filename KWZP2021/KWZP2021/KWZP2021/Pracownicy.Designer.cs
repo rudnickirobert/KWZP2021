1ec6adce7e363ee9ec8dd6f83252565e10a42975
Bartek <bartoszgebicki@gmail.com>
2021-05-05 21:53:53 +0200

    Guzik powrót Pracownicy

--- KWZP2021/KWZP2021/KWZP2021/Pracownicy.Designer.cs
+++ KWZP2021/KWZP2021/KWZP2021/Pracownicy.Designer.cs
@@ -119,6 +119,7 @@ namespace KWZP2021
             this.button1.TabIndex = 15;
             this.button1.Text = "Powrót";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
